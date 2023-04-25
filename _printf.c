8e9921a8a3145592385b52b8845f13e99fc9db29
Isaac Frank <codingwithisaac@gmail.com>
2023-04-25 13:24:19 +0100

    printf scafolding

---
+++ _printf.c
@@ -0,0 +1,8 @@
+/**
+ * _printf - Custom made printf function
+ * @format: format.
+ * Return: Number of bytes printed
+ */
+int _printf(const char *format, ...)
+{
+}
--- main.h
+++ main.h
@@ -1,5 +1,8 @@
 #ifndef MAIN_H
 #define MAIN_H
+#include <stdarg.h>
+#include <stdio.h>
+#include <unistd.h>
 
 int _printf(const char *format, ...);
 
